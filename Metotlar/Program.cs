3cd6534defea4feeac63abbd8c0cadbe46eafef5
BRIGHT <BRIGHT@DESKTOP-ST51CUU>
2024-01-16 21:56:07 +0300

    Add project files.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// Metot yazmak kod tekrarını engeller
+// İş yapan ufak kod parçacıklarına metot denir
+// Metotlar aldıkları belirli değerler ile ilgili işler yapıp size sonuçlar verebilmektedir.
+Test();
+Test();
+
+
+
+Random rnd = new Random();
+int sayi = rnd.Next();
+
+
+// her metodun bir imzası olması gerekmektedir.
+// erişim belirleyici
+// static yada static olmalışı
+// geri dönüş tipi
+// Adı
+// Aldığı parametreler
+// Gövde
+
+
+// Geri dönüş tipi olmayan ve parametre almayan metot
+// Geri görüş tipi olan ve parametre almayan metot
+static void Test()
+{
+    Console.WriteLine("Test");
+}
+
+// Parametre almayan ve geriye değer döndüren metot
+static int Test2()
+{
+    return 10;
+
+}
+
+// int tipinde değer alan ve geriye değer döndürmeyen metot
+static void Test3(int a)
+{
+
+
+}
+
+// Parametre alan ve geriye değer döndüren metot
+static string Test4(string deger)
+{
+
+    return "random değer";
+}
---
+++ MetotÖrnekler/Program.cs
@@ -0,0 +1,72 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Örnek 1 : Ekrandan aldığı değeri ters çevirip ekrana yazan örnek
+using System.Collections.Concurrent;
+using System.Globalization;
+
+//Console.WriteLine("Lütfen bir yazı yazınız");
+//string yazi = Console.ReadLine();
+
+
+//string metotdanGelen = TersCevir(yazi);
+//Console.WriteLine(metotdanGelen);
+
+int sonuc =KacKelime("kaç kelime olduğunu bul");
+Console.WriteLine(sonuc);
+
+
+Console.WriteLine(RakamSil("5445İST888rakambesi11322ktas"));
+
+
+// 
+
+// 
+// girilen bir cümlenin içeriisndeki rakamları temizleyin uygulama
+
+
+//bir cümlenin içerisinde kaç adet kelime geçtiğini söyleyen bir metot yazınız
+
+
+static string RakamSil(string deger) {
+
+    //asci rakam konumları bulunur kontrol edilir
+    // tüm alfabe yazılır ve her bir harf o alfabe üzerinden kontrol edeilir.
+    // char.İsDigit gibi fonksiyonlar kullanılır.
+
+    string temiz = "";
+    for (int i = 0; i < deger.Length; i++)
+    {
+
+        if (char.IsLetter(deger[i]))
+        {
+            temiz += deger[i];
+        }
+    }
+    return temiz;
+
+
+}
+static int KacKelime(string deger)
+{
+    int kelime = 1;
+    for (int i = 0; i < deger.Length; i++)
+    {
+        if (deger[i] == ' ')
+        {
+            kelime++;
+        }
+    }
+    return kelime;
+
+}
+static string TersCevir(string deger)
+{
+    string yeniDeger = "";
+    for (int i = deger.Length - 1; i >= 0; i--)
+    {
+        yeniDeger += deger[i];
+
+    }
+
+    return yeniDeger;
+}
